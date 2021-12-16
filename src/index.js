7a2092377f1de5a6f5f838a42da6bdb56f2f3829
alta7v <aquila.alta7v@gmail.com>
2021-12-16 15:30:09 +0900

    add ret button

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import "./styles.css";
 
-const onClickAdd = () => {
-  // テキストボックスの値を取得し、初期化する
-  const inputText = document.getElementById("add-text").value;
-  document.getElementById("add-text").value = "";
-  //alert(inputText);
-
+const addTodo = (text) => {
   // li
   const li = document.createElement("li");
 
@@ -17,7 +12,7 @@ const onClickAdd = () => {
   // div2
   const div2 = document.createElement("div");
   div2.className = "todo-status";
-  div2.innerText = inputText;
+  div2.innerText = text;
   div.appendChild(div2);
 
   // complete button
@@ -25,7 +20,7 @@ const onClickAdd = () => {
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
     // Text を 取得する
-    const text = completeButton.parentNode.firstChild.innerText;
+    const completeText = completeButton.parentNode.firstElementChild.innerText;
 
     // Element 作成
     const completeLi = document.createElement("li");
@@ -34,16 +29,22 @@ const onClickAdd = () => {
     completeLi.appendChild(completeDiv);
     const completeDiv2 = document.createElement("div");
     completeDiv2.className = "todo-status";
-    completeDiv2.innerText = text;
+    completeDiv2.innerText = completeText;
     completeDiv.appendChild(completeDiv2);
-    const completeRetButton = document.createElement("button");
+    const completeRetButton = document.createElement("button"); // 戻るbutton
     completeRetButton.innerText = "戻る";
     completeDiv.appendChild(completeRetButton);
+    completeRetButton.addEventListener("click", () => {
+      const text = completeButton.parentNode.firstElementChild.innerText;
+
+      addTodo(text);
+      deleteCompleteList(completeRetButton);
+    });
 
     // 完了リストに追加する
     addFromCompleteList(completeLi);
     // ボタンの親 li タグを消す
-    deleteFromIncompleteList(completeButton);
+    deleteIncompleteList(completeButton);
   });
   div.appendChild(completeButton);
 
@@ -52,7 +53,7 @@ const onClickAdd = () => {
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
     // ボタンの親 li タグを消す
-    deleteFromIncompleteList(deleteButton);
+    deleteIncompleteList(deleteButton);
   });
   div.appendChild(deleteButton);
 
@@ -60,14 +61,29 @@ const onClickAdd = () => {
 };
 
 // 未完 リストから削除する
-const deleteFromIncompleteList = (targetElement) => {
+const deleteIncompleteList = (targetElement) => {
   const parrentdiv = targetElement.parentNode;
   const parrentli = parrentdiv.parentNode;
   document.getElementById("incomplete-list").removeChild(parrentli);
 };
 
+// 未完 リストから削除する
+const deleteCompleteList = (targetElement) => {
+  const parrentdiv = targetElement.parentNode;
+  const parrentli = parrentdiv.parentNode;
+  document.getElementById("complete-list").removeChild(parrentli);
+};
+
 const addFromCompleteList = (targetElement) => {
   document.getElementById("complete-list").appendChild(targetElement);
+};
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  //alert(inputText);
+  addTodo(inputText);
 };
 
 document.getElementById("add-button").addEventListener("click", onClickAdd);
